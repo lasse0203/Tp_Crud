3fecc53f9bf3997d4653e88ca1903fdea66abc70
ikhlasse <ikhlasse@gmail.com>
2023-09-08 16:45:54 +0200

    exo

---
+++ src/main/java/com/example/tp_crud/data/FakeDB.java
@@ -0,0 +1,10 @@
+package com.example.tp_crud.data;
+import com.example.tp_crud.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeDB {
+        public static final List<User> users = new ArrayList<>();
+    }
+
